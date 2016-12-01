57c6b0ce4d57ffaebe1fadc3c342f8beac429f62
pakhoa <pakhoa@apcs.vn>
2016-12-01 01:44:25 -0800

    update activeadmin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,20 +4,20 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-
-    can :read, Post
-    can :create, Post
-    can :update, Post do |post|
-      post.try(:user) == user
-    end
-    can :destroy, Post do |post|
-      post.try(:user) == user
-    end
-
-    can :create, Comment
-    can :destroy, Comment do |post|
-      post.try(:user) == user
-    end
-
+    if user.is_admin?
+      can :manage, :all
+    else
+      can :read, Post
+      can :create, Post
+      can :update, Post do |post|
+      	post.try(:user) == user
+      end
+      can :destroy, Post do |post|
+      	post.try(:user) == user
+      end
+      can :create, Comment
+      can :destroy, Comment do |post|
+      	post.try(:user) == user
+      end
   end
 end
